9baa7ebdbd4b931274db75a52d3bf8a07d0c476b
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2026-04-25 11:14:47 +0200

    Aggregation: Matching & Grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,13 @@
 const Tour = require("../models/tourModel");
 const APIFeatures = require("../utils/apiFeatures");
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingsAverage,price";
+  req.query.fields = "name,price,ratingsAverage,summary,difficulty";
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     const features = new APIFeatures(Tour.find(), req.query)
@@ -32,79 +39,6 @@ exports.getAllTours = async (req, res) => {
     });
   }
 };
-
-// exports.getAllTours = async (req, res) => {
-//   try {
-//     // console.log(req.query);
-//     // const query = await Tour.find()
-//     //   .where("duration")
-//     //   .equals(5)
-//     //   .where("difficulty")
-//     //   .equals("easy");
-//     // BUILD QUERY
-//     // 1A) Filtering
-//     const queryObj = { ...req.query };
-//     const excludedFields = ["page", "sort", "limit", "fields"];
-//     excludedFields.forEach((el) => delete queryObj[el]);
-//     // console.log(req.query, queryObj);
-
-//     // 1B) Advanced Filtering
-//     let queryStr = JSON.stringify(queryObj);
-//     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-//     // console.log(queryStr);
-
-//     const filters = JSON.parse(queryStr);
-
-//     let query = Tour.find(filters);
-
-//     // 2) Sorting
-//     if (req.query.sort) {
-//       const sortBy = req.query.sort.split(",").join(" ");
-//       console.log(sortBy);
-//       query = query.sort(sortBy);
-//     } else {
-//       query = query.sort("-createdAt");
-//     }
-
-//     // 3) Limiting Fields
-//     if (req.query.fields) {
-//       const fields = req.query.fields.split(",").join(" ");
-//       query = query.select(fields);
-//     } else {
-//       query = query.select("-__v");
-//     }
-
-//     // 4) Pagination
-//     const page = +req.query.page || 1;
-//     const limit = +req.query.limit || 100;
-//     const skip = limit * (page - 1);
-
-//     query = query.skip(skip).limit(limit);
-
-//     // EXECUTE QUERY
-//     const tours = await query;
-
-//     if (req.query.page && page > 1) {
-//       if (!tours.length) {
-//         throw new Error("This Page Doesn't Exist");
-//       }
-//     }
-
-//     // SEND RESPONSE
-//     res.status(200).json({
-//       status: "success",
-//       results: tours.length,
-//       data: {
-//         tours,
-//       },
-//     });
-//   } catch (err) {
-//     res.status(404).json({
-//       status: "fail",
-//       message: err.message,
-//     });
-//   }
-// };
 
 exports.getTour = async (req, res) => {
   try {
@@ -169,6 +103,45 @@ exports.deleteTour = async (req, res) => {
     res.status(204).json({
       status: "success",
       data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.getToursStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: "$difficulty" },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: "$ratingsQuantity" },
+          avgRating: { $avg: "$ratingsAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: "EASY" } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        stats,
+      },
     });
   } catch (err) {
     res.status(404).json({
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -6,7 +6,8 @@ class APIFeatures {
 
   filter() {
     const queryObj = { ...this.queryString };
-    ["page", "sort", "limit", "fields"].forEach((el) => delete queryObj[el]);
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
     let queryStr = JSON.stringify(queryObj);
 
