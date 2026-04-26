ae7c88bdd60fd9c837ea387040439c474b49445a
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2026-04-26 14:39:53 +0200

    Handling DB errors as operational errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,66 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const [[field, value]] = Object.entries(err.keyValue);
+  const message = `Duplicate field value: ${value}. Please use another ${field}!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errorsMessages = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errorsMessages.join(". ")}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, truested error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unkown error: don't leak error details
+  } else {
+    // 1) log error
+    console.log("ERROR 💥", err);
+
+    // 2) send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
-  err.status = err.stats || "error";
+  err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    console.log(error);
+    if (err.name === "CastError") error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === "ValidationError") error = handleValidationErrorDB(error);
+    sendErrorProd(error, res);
+  }
 };
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require("../models/tourModel");
 const APIFeatures = require("../utils/apiFeatures");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = "5";
@@ -36,6 +37,11 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    throw new AppError("No tour found with that ID", 404);
+  }
+
   res.status(200).json({
     status: "success",
     data: {
@@ -60,6 +66,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+
+  if (!tour) {
+    throw new AppError("No tour found with that ID", 404);
+  }
+
   res.status(200).json({
     status: "success",
     data: {
@@ -69,7 +80,12 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    throw new AppError("No tour found with that ID", 404);
+  }
+
   res.status(204).json({
     status: "success",
     data: null,
