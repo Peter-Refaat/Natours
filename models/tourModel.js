e17dfc2c800085812238d1f1c54839db61e491ca
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2026-04-26 09:46:17 +0200

    Built-in & Custom Validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,14 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a name"],
       unique: true,
       trim: true,
+      maxLength: [
+        40,
+        "A tour name must have less than or equal to 40 characters",
+      ],
+      minLength: [
+        10,
+        "A tour name must have more than or equal to 10 characters",
+      ],
     },
     slug: String,
     duration: {
@@ -21,10 +29,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either easy, medium, difficult",
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "Rating must be above 1"],
+      max: [5, "Rating must be below 5"],
     },
     ratingsQuantity: {
       type: Number,
@@ -34,7 +48,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, "A tour must have a price"],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only point to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: "Discount price ({VALUE}) must be lower than actual price",
+      },
+    },
     summary: {
       type: String,
       trim: true,
