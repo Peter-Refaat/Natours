7270cc79d4ddced82119a0dc29a186024b5b5131
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2026-05-03 12:35:09 +0300

    Hashing password, JWT

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,25 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, "Please confirm your password"],
+    validate: {
+      // This only works on CREATE & SAVE!!!
+      validator: function (val) {
+        return val === this.password;
+      },
+      message: "Passwords are not the same",
+    },
   },
+});
+
+userSchema.pre("save", async function () {
+  // only run if the password is actually modified
+  if (!this.isModified("password")) return;
+
+  // Hash the password with the cost of 12 O(2^salt)
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete the passwordConfirm field
+  this.passwordConfirm = undefined;
 });
 
 module.exports = mongoose.model("User", userSchema);
