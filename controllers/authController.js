d521a79d9d8aa697712e59d41c60358d28751caa
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2026-05-05 09:52:37 +0300

    User roles & Permissions

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
     passwordChangedAt: req.body.passwordChangedAt,
+    role: req.body.role,
   });
 
   const token = signToken(newUser._id);
@@ -94,3 +95,15 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = curUser; // Will be useful in the future
   next();
 });
+
+exports.restrictTo =
+  (...roles) =>
+  (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You don't have permission to perform this action", 403),
+      );
+    }
+
+    next();
+  };
