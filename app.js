39fe1622202e3089b147bdb2c999f337ec5a0a62
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2026-05-16 16:54:11 +0300

    Implemented NoSQL & XSS Data Sanitization

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express, { json, static as expressStatic } from "express";
 import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize"
 import { rateLimit } from "express-rate-limit";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
+import xssSanitizer from "./utils/deepSanitize.js";
 import AppError from "./utils/appError.js";
 import globalErrorHandler from "./controllers/errorController.js";
 import tourRouter from "./routes/tourRoutes.js";
@@ -14,29 +17,43 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-//GLOBAL MIDDLEWARES
-app.use(json());
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Body parser, reading data from the body into req.body
+app.use(json({ limit: "10kb" }));
+
+// Data Sanitization agains NoSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS (cleans req.body, req.query & req.params)
+app.use(xssSanitizer);
+
+// Serving static files
 app.use(expressStatic(`${__dirname}/public`));
 
+// Limit requests from the same IP
 const limiter = rateLimit({
   limit: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!",
 });
-
 app.use("/api", limiter);
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log("HEADERSSSS", req.headers);
+  // console.log("HEADERSSSS", req.headers);
   next();
 });
 
-//ROUTES
+// 2) ROUTES
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
---
+++ utils/deepSanitize.js
@@ -0,0 +1,54 @@
+import sanitizeHtml from "sanitize-html";
+
+// recursive function to sanitize deeply nested objects against XSS attacks
+const deepSanitize = function (val) {
+  if (typeof val === "string") {
+    return sanitizeHtml(val, {
+      allowedTags: [
+        "b",
+        "i",
+        "em",
+        "strong",
+        "ul",
+        "ol",
+        "li",
+        "p",
+        "br",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "blockquote",
+        "a",
+        "img",
+      ],
+      allowedAttributes: {
+        a: ["href", "target"],
+        img: ["src", "alt", "width", "height"],
+      },
+      allowedSchemes: ["http", "https"],
+    });
+  }
+  if (Array.isArray(val)) {
+    return val.map((item) => deepSanitize(item));
+  }
+  if (typeof val === "object" && val !== null) {
+    Object.keys(val).forEach((key) => {
+      val[key] = deepSanitize(val[key]);
+    });
+    return val;
+  }
+  return val;
+};
+
+const xssSanitizer = (req, res, next) => {
+  if (req.body) req.body = deepSanitize(req.body);
+  if (req.query) req.query = deepSanitize(req.query);
+  if (req.params) req.params = deepSanitize(req.params);
+
+  next();
+}
+
+export default xssSanitizer;
