a58a413578e6d9bb23c60fd11844edd27a5efd6d
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2026-09-03 18:36:21 +0300

    Implemented /me endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,6 +13,11 @@ const filterObj = (obj, ...allowedFields) => {
   return ret;
 };
 
+export const getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 export const updateMe = catchAsync(async (req, res, next) => {
   // 1) Create an error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,7 @@ import {
   updateUser,
   deleteUser,
   deleteMe,
+  getMe,
 } from "../controllers/userController.js";
 import {
   signup,
@@ -25,6 +26,7 @@ router.post("/forgotPassword", forgotPassword);
 router.patch("/resetPassword/:token", resetPassword);
 router.patch("/updateMyPassword", protect, updatePassword);
 
+router.get("/me", protect, getMe, getUser);
 router.patch("/updateMe", protect, updateMe);
 router.delete("/deleteMe", protect, deleteMe);
 
