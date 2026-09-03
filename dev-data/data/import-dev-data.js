c9454e914f036b5fb681d8e87e0c56b9edb7d2fd
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2026-09-03 19:13:45 +0300

    Imported Real Data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,9 +2,11 @@ import { readFileSync } from "fs";
 import { setServers } from "dns";
 import { connect } from "mongoose";
 import { config } from "dotenv";
-import Tour from "../../models/tourModel.js";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import Tour from "../../models/tourModel.js";
+import User from "../../models/userModel.js";
+import Review from "../../models/reviewModel.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -26,10 +28,14 @@ connect(DB)
   });
 
 const tours = JSON.parse(readFileSync(`${__dirname}/tours.json`, "utf-8"));
+const users = JSON.parse(readFileSync(`${__dirname}/users.json`, "utf-8"));
+const reviews = JSON.parse(readFileSync(`${__dirname}/reviews.json`, "utf-8"));
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log("Data Successfuly loaded!");
   } catch (err) {
     console.log(err);
@@ -40,6 +46,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data Successfuly Deleted!");
   } catch (err) {
     console.log(err);
