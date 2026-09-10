57eb1a01beda2b5a5885d7367aa41faa396fa707
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2026-09-10 12:44:22 +0300

    Built overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,16 @@
-export const getOverview = (req, res) => {
+import Tour from "../models/tourModel.js";
+import catchAsync from "../utils/catchAsync.js";
+
+export const getOverview = catchAsync(async (req, res, next) => {
+  // 1) get tours data from collection
+  const tours = await Tour.find();
+  // 2) Build template
+  // 3) Render that template using tours data from 1)
   res.status(200).render("overview", {
     title: "All Tours",
+    tours,
   });
-};
+});
 
 export const getTour = (req, res) => {
   res.status(200).render("tour", {
