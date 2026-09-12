c6468351b329727ab08793d2033ccd6edf205035
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2026-09-12 23:33:08 +0300

    Sending welcome emails

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 import catchAsync from "../utils/catchAsync.js";
 import AppError from "../utils/appError.js";
-import sendEmail from "../utils/email.js";
+import Email from "../utils/email.js";
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -47,7 +47,9 @@ export const signup = catchAsync(async (req, res, next) => {
     role: req.body.role,
     photo: req.body.photo,
   });
-
+  const url = `${req.protocol}://${req.get("host")}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
   createSendToken(newUser, 201, res);
 });
 
@@ -177,11 +179,11 @@ export const forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: "Your password reset token (valid for 10 min)",
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: "Your password reset token (valid for 10 min)",
+    //   message,
+    // });
 
     res.status(200).json({
       status: "success",
--- utils/email.js
+++ utils/email.js
@@ -1,23 +1,60 @@
+import path from "path";
 import { createTransport } from "nodemailer";
+import pug from "pug";
+import { convert } from "html-to-text";
+import { fileURLToPath } from "url";
 
-const sendEmail = async (options) => {
-  const transporter = createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  // 2) Define the email options
-  const mailOptions = {
-    from: "Peter Refaat <peter@natours.dev>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
-  // 3) Actually send the email
-  await transporter.sendMail(mailOptions);
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-export default sendEmail;
+export default class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `Peter Refaat <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      console.log("TODO MAILS IN PRODUCTION");
+      return 1;
+    }
+
+    return createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // send the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: convert(html, {
+        wordwrap: false,
+      }),
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to the Natours Family!");
+  }
+}
