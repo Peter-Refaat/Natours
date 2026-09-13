a14bffe477c2c64083d5e8345d608082df51ff08
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2026-09-13 12:10:30 +0300

    Modelling the bookings

---
+++ models/bookingModel.js
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+
+const bookingSchema = new Schema({
+  tour: {
+    type: Schema.ObjectId,
+    ref: "Tour",
+    required: [true, "Booking must belong to a tour"],
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: "User",
+    required: [true, "Booking must belong to a user"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Booking must have a price"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+bookingSchema.pre(/^find/, function () {
+  this.populate("user").populate({
+    path: "tour",
+    select: "name",
+  });
+});
+
+export default model("Booking", bookingSchema);
